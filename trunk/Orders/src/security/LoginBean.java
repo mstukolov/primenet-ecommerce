abeb95c3dcdc8e53fb40cb4d546e6fd61fe9503e
maxim.stukolov@tbwa.ru <maxim.stukolov@tbwa.ru@b42231b6-128c-8da6-6354-0c6420d7f833>
2014-05-23 19:01:43 +0000

    Issue 24:	Подключить систему аутентификации Spring Security

---
+++ trunk/Orders/src/security/LoginBean.java
@@ -0,0 +1,73 @@
+package security;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+@ManagedBean(name="loginBean")
+@SessionScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+
+    public LoginBean() {
+    }
+
+    public Boolean isAuthenticated(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && !(authentication instanceof AnonymousAuthenticationToken) && authentication.isAuthenticated();
+    }
+    public String ssUserName(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getName();
+    }
+
+    public String doLogin() throws IOException, ServletException {
+        ExternalContext context = FacesContext.getCurrentInstance()
+                .getExternalContext();
+
+        RequestDispatcher dispatcher = ((ServletRequest) context.getRequest())
+                .getRequestDispatcher("/j_spring_security_check?j_username=" + username
+                        + "&j_password=" + password);
+
+        dispatcher.forward((ServletRequest) context.getRequest(),
+                (ServletResponse) context.getResponse());
+
+        FacesContext.getCurrentInstance().responseComplete();
+        // It's OK to return null here because Faces is just going to exit.
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
